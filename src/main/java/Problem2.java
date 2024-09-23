517b549a8981fb1d935847b41ffb3b437a2279a8
dennisranishstudent <112589636+dennisranishstudent@users.noreply.github.com>
2024-09-23 11:43:43 -0400

    Commit

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,9 +1,26 @@
 
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-        // TODO: Implement the insert method
-        // This method should insert a new node with the given value at the specified position
-        // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        if(position <= 1) {
+            ListNode addNode = new ListNode(val);
+            addNode.next = head;
+            return addNode;
+        }
+
+        if(head == null) return new ListNode(val);
+
+        ListNode beforeInsert = head;
+
+        for(int i = 0; i < position - 2; i++) {
+            if(beforeInsert.next == null) break;
+            beforeInsert = beforeInsert.next;
+        }
+
+        ListNode afterInsert = beforeInsert.next;
+
+        beforeInsert.next = new ListNode(val);
+        beforeInsert.next.next = afterInsert;
+
+        return head;
     }
 }
